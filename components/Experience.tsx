b0d7cb9edb93a7b30ef3519671460d66cf0cd481
Aditya-Srivastava <pks85715@gmail.com>
2024-10-15 02:05:35 +0530

    event file added

---
+++ components/Experience.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+import { workExperience } from "@/data";
+import { Button } from "./ui/MovingBorders";
+import Link from "next/link";
+
+const Experience = () => {
+  return (
+    <div className="py-10 w-full" id="members">
+      <h1 className="heading">
+        Our <span className="text-purple">Cordinators</span> at{" "}
+        <span className="text-purple">Signodes</span>
+      </h1>
+
+      <div className="w-full mt-10 grid lg:grid-cols-4 grid-cols-1 gap-10">
+        {workExperience.map((card) => (
+          <Button
+            key={card.id}
+            //   random duration will be fun , I think , may be not
+            duration={Math.floor(Math.random() * 10000) + 10000}
+            borderRadius="1.75rem"
+            style={{
+              //   add these two
+              //   you can generate the color from here https://cssgradient.io/
+              background: "rgb(4,7,29)",
+              backgroundColor:
+                "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+              // add this border radius to make it more rounded so that the moving border is more realistic
+              borderRadius: `calc(1.75rem* 0.96)`,
+            }}
+            // remove bg-white dark:bg-slate-900
+            className="flex-1 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+          >
+            <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
+              <img
+                src={card.thumbnail}
+                alt={card.thumbnail}
+                className="lg:w-32 md:w-20 w-16"
+              />
+              <div className="lg:ms-5">
+                <h1 className="text-start text-xl md:text-2xl font-bold">
+                  {card.title}
+                </h1>
+                <p className="text-start text-white-100 mt-3 font-semibold">
+                  {card.desc}
+                </p>
+                <div className="flex items-center justify-between mt-7 mb-3">
+                  <div className="flex items-center gap-2">
+                    {card.socials.map((icon, index) => (
+                      <div
+                        key={index}
+                        className="border border-white/[.2] rounded-full bg-white lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      >
+                        <a key={index} href={card.socialslinks[index]}>
+                          <img src={icon} alt="icon5" className="p-2" />
+                        </a>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
+
+
+
+{/* <a href="mailto:signodes_iot@niet.co.in" className="mb-5"></a> */}
---
+++ data/index.ts
@@ -0,0 +1,316 @@
+export const navItems = [
+  {name: "Home" , link: "#home"},
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Members", link: "#members" },
+  { name: "Contact", link: "#contact" },
+
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "About Signodes",
+    description:
+      "At Signodes Club, we are passionate about exploring the exciting world of the Internet of Things (IoT). Our club is dedicated to fostering innovation through a hands-on approach to both hardware and software projects. Whether you're a coding enthusiast, a budding engineer, or simply curious about technology, Signodes Club is the perfect place for you! We organize a variety of activities, including hackathons, workshops, and events, designed to provide students with the exposure they need to dive deep into the latest technologies. Our collaborative environment encourages creativity and teamwork, allowing members to develop practical skills while tackling real-world challenges.Join us as we explore the endless possibilities of IoT, enhance our technical skills, and build a community of forward-thinking individuals ready to shape the future of technology",
+
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/grid.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Welcoming  ",
+    description: "Everyone to shine",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "We embrace",
+    description: "technologies and learning",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "/grid.svg",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Achievements",
+    description:
+      "Our members have won 10+ hackathons,showcasing their potential,we empower students to create smart solutions for real-world challenges",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    // /b4.svg
+    spareImg: "",
+  },
+
+  {
+    id: 5,
+    title: "Our Activities",
+    description:
+      "At Signodes Club, we engage in a variety of exciting events and initiatives that empower our members to push the boundaries of innovation. Our flagship events, such as Iotron and Nexus, serve as platforms for collaboration, creativity, and competition, allowing students to showcase their skills and ideas on a larger stage , In addition to participating in competitions, our members actively contribute to research papers and develop impactful IoT projects. From smart street lighting systems that enhance urban efficiency to web applications that streamline everyday tasks, our projects reflect our commitment to real-world solutions. Notably, Ezeagro our agriculture-based initiative demonstrates how IoT can transform traditional farming practices.",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "",
+    spareImg: "/grid.svg",
+  },
+  // {
+  //   id: 6,
+  //   title: "Connect with us",
+  //   description: "",
+  //   className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+  //   imgClassName: "",
+  //   titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+  //   img: "",
+  //   spareImg: "",
+  // },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Servo Robotic Arm",
+    des: "It is an autonomous robotic arm prototype for different tasks",
+    img: "/servo.jpeg",
+    // iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "",
+    pintitle: "ROBOTIC ARM",
+  }, 
+  {
+    id: 2,
+    title: "Robot",
+    des: "Mark1 is an greeting robot designed to welcome and engage all members",
+    img: "/mark1.jpeg",
+    // iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "",
+    pintitle: "Robot",
+  },
+  {
+    id: 3,
+    title: "Autonomous Car",
+    des: "It is the prototype of autonomous car concept",
+    img: "/car.jpeg",
+    // iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "",
+    pintitle: "AUTONOMOUS CAR",
+  },
+  {
+    id: 4,
+    title: "Smart city",
+    des: "This is the entire city with smart and automatic management system",
+    img: "/city.jpeg",
+    // iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "",
+    pintitle: "SMART CITY",
+  },
+];
+
+// export const testimonials = [
+//   {
+//     quote:
+//       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//     name: "Michael Johnson",
+//     title: "Director of AlphaStream Technologies",
+//   },
+//   {
+//     quote:
+//       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//     name: "Michael Johnson",
+//     title: "Director of AlphaStream Technologies",
+//   },
+//   {
+//     quote:
+//       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//     name: "Michael Johnson",
+//     title: "Director of AlphaStream Technologies",
+//   },
+//   {
+//     quote:
+//       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//     name: "Michael Johnson",
+//     title: "Director of AlphaStream Technologies",
+//   },
+//   {
+//     quote:
+//       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//     name: "Michael Johnson",
+//     title: "Director of AlphaStream Technologies",
+//   },
+// ];
+
+export const companies = [
+  {
+    id: 1,
+    name: "FirstThing",
+    img: "/first.jpeg",
+    nameImg: "",
+  },
+  {
+    id: 2,
+    name: "Newgen",
+    img: "/newgen.png",
+    nameImg: "",
+  },
+  {
+    id: 3,
+    name: "kpmg",
+    img: "/kpmg.jpeg",
+    nameImg: "",
+  },
+  {
+    id: 4,
+    name: "kimbal",
+    img: "/kimbal.jpeg",
+    nameImg: "",
+  },
+  {
+    id: 5,
+    name: "brainwave",
+    img: "/brainwave.svg",
+    nameImg: "",
+  },
+  {
+    id: 6,
+    name: "evpi",
+    img: "/evpi.jpeg",
+    nameImg: "",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Mayank Deep Khare",
+    desc: "FACULTY COORDINATOR , H.O.D CSE-IOT",
+    className: "md:col-span-2",
+    thumbnail: "/mayank-sir.jpeg",
+    socials: ["/linkedin.svg", "/mail.svg"],
+    socialslinks: [
+      "https://www.linkedin.com/in/mayank-deep-khare-23871640?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      "mailto:hodiot@niet.co.in",
+    ],
+  },
+  {
+    id: 2,
+    title: "Addite Matttoo",
+    desc: "FACULTY COORDINATOR , DEPUTY H.O.D CSE-M.TECH INTEGRATED",
+    className: "md:col-span-2",
+    thumbnail: "aditee-mam.jpeg",
+    socials: ["/linkedin.svg", "/mail.svg"],
+    socialslinks: [
+      "https://www.linkedin.com/in/aditee-m-5841b4140?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      "mailto:aditeemattoo@niet.co.in",
+    ],
+  },
+  {
+    id: 3,
+    title: "Prashant Kumar Signh",
+    desc: "IOT ENGINEER AT EPVI , INTERN AT FirstThing , PRESIDENT AT SIGNODES CLUB ,  EMERGIN'23 WINNER , HACK-A-THON IMS'23 WINNER , HACKX-NIET'23 WINNER , SIH-FINALIST'23 , IILM WINNER , HARD-WAR UPES FINALIST , BIC'23 TOP 5 IN COLLEGE CATEGORY , CSE-IOT (4rd-year)",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "prashant.jpeg",
+    socials: ["/github2.svg", "/linkedin.svg", "/mail.svg"],
+    socialslinks: [
+      "https://github.com/Prashant9876",
+      "https://www.linkedin.com/in/prashant74887?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      "mailto:0211csio010@niet.co.in",
+    ],
+  },
+  {
+    id: 4,
+    title: "Amiya Krishan Gupta",
+    desc: "INTERN AT BRAINWAVE TECHNOLOGIES , HARDWARE HEAD AT SIGNODES CLUB , EMERGIN'23 WINNER , HACK-A-THON IMS'23 WINNER , HACKX-NIET'23 WINNER , SIH-FINALIST'23 , IILM WINNER , HARD-WAR UPES FINALIST , BIC'23 TOP 5 IN COLLEGE CATEGORY , AWARD WINNING INNOVATOR , CSE-IOT (4rd-year)",
+    className: "md:col-span-2",
+    thumbnail: "amy.jpeg",
+    socials: ["/github2.svg", "/linkedin.svg", "/mail.svg"],
+    socialslinks: [
+      "https://github.com/Amiya200",
+      "https://www.linkedin.com/in/amiya-krishna-gupta-b7635423b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      "mailTo:0211csio002@niet.co.in",
+    ],
+  },
+  // {
+  //   id: 5,
+  //   title: "Arpit Raj Barnwal",
+  //   desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui deleniti tempore nesciunt natus iusto quos impedit hic sapiente nulla laudantium",
+  //   className: "md:col-span-2",
+  //   thumbnail: "/exp1.svg",
+  //   socials: ["/github2.svg", "/linkedin.svg", "/mail.svg"],
+  // },
+  {
+    id: 6,
+    title: "Piyush Kumar Jha",
+    desc: "EVENT HEAD AT SIGNODES CLUB , EMERGIN'23 WINNER , HACK-A-THON IMS'23 WINNER , HACKX-NIET'23 WINNER , SIH-FINALIST'23 , IILM WINNER , HARD-WAR UPES FINALIST , BIC'23 TOP 5 IN COLLEGE CATEGORY , CSE-IOT (4th-year)",
+    className: "md:col-span-2",
+    thumbnail: "piyush.jpeg",
+    socials: ["/github2.svg", "/linkedin.svg", "/mail.svg"],
+    socialslinks: [
+      "https://github.com/Emiwine",
+      "http://www.linkedin.com/in/piyushjha2506",
+      "mailTo:piyushjha416@gmail.com",
+    ],
+  },
+  {
+    id: 7,
+    title: "Aryan Srivastav",
+    desc: "SOCIAL MEDIA HEAD AT SIGNODES , PRESIDENT AT MEGAPIXELS DESIGNER , CSE-IOT (4rd-year)",
+    className: "md:col-span-2",
+    thumbnail: "aryan.jpeg",
+    socials: ["/github2.svg", "/linkedin.svg", "/mail.svg"],
+    socialslinks: [
+      "https://github.com/Anonymousurfer08",
+      "https://www.linkedin.com/in/aryan-srivastav-b3a29524b/",
+      "mailTo:aryansrivastavone@gmail.com",
+    ],
+  },
+  {
+    id: 8,
+    title: "Sumesh Sarkar",
+    desc: "VICE-PRESIDENT AT SIGNODES CLUB , SECOND RUNNER-UP IOTRON 2.0 , HACKX-NIET'23 WINNER , HACKFEEST HACKATHON AT GEEKS-FOR-GEEKS PATICIPANT , CSE-IOT (3rd-year)",
+    className: "md:col-span-2",
+    thumbnail: "sumesh.jpeg",
+    socials: ["/github2.svg", "/linkedin.svg", "/mail.svg"],
+    socialslinks: [
+      "https://github.com/Sumesh-skr",
+      "https://www.linkedin.com/in/sumesh-skr",
+      "mailTo:0221csiot105@niet.co.in",
+    ],
+  },
+  {
+    id: 9,
+    title: "Rishi Ranjan",
+    desc: "VICE-PRESIDENT AT SIGNODES , BUILDING COHORTLAB , GDG NIET TECHNICAL LEAD'24 , AI/ML ENTHUSIAST , CSE-IOT(3rd year)",
+    className: "md:col-span-2",
+    thumbnail: "rishi.jpeg",
+    socials: ["/github2.svg", "/linkedin.svg", "/mail.svg"],
+    socialslinks: [
+      "",
+      "https://www.linkedin.com/in/rishi-rih?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      "mailTo:0221csiot091@niet.co.in",
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/insta.svg",
+    socialslinks: [
+      "https://www.instagram.com/niet_signodes?igsh=dDFzOXkwNHU5dWM=",
+    ],
+  },
+  {
+    id: 3,
+    img: "/linkedin.svg",
+    socialslinks: ["https://www.linkedin.com/company/nietsignodes/"],
+  },
+];
